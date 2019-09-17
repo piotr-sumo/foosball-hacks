a724b640b1e1fcc35f2c4141e11f7f3f583667ee
Piotr Grabowski <pgrabowski@sumologic.com>
2019-09-17 16:48:41 +0200

    intial camera work

---
+++ camera_api.py
@@ -0,0 +1,17 @@
+from flask import Flask, escape, request
+from camera import ContinousCamera
+import os
+import secrets
+
+app = Flask(__name__)
+
+camera = ContinousCamera()
+
+@app.route("/save_recording")
+def save_recording():
+    filename = f"recording{secrets.token_hex(15)}"
+
+    camera.dump_to_file(filename)
+    os.system(f"./camera_process.sh {filename} &")
+
+    return filename
