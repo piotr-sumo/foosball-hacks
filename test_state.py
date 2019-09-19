b70ba3fd212785b02b9abd133f05f8df9c4d4c89
Piotr Glazar <pglazar@sumologic.com>
2019-09-19 11:43:08 +0200

    Main game file and some tests

--- test_state.py
+++ test_state.py
@@ -39,6 +39,41 @@ class StateTest(TestCase):
         self.assertEqual(len(test_listener.get_captured_events()), 1)
         self.assertTrue(isinstance(test_listener.get_captured_events()[0], BlueGoal))
 
+    def test_should_count_blue_goals(self):
+        # given
+        state = State()
+
+        # when
+        state.blue_scores()
+
+        # then
+        self.assertEqual(state.get_blue_goals(), 1)
+        self.assertEqual(state.get_current_score().get_blue_goals(), 1)
+
+    def test_should_count_red_goals(self):
+        # given
+        state = State()
+
+        # when
+        state.red_scores()
+
+        # then
+        self.assertEqual(state.get_red_goals(), 1)
+        self.assertEqual(state.get_current_score().get_red_goals(), 1)
+
+    def test_should_attach_listeners(self):
+        # given
+        test_listener = TestStateListener()
+        state = State()
+
+        # when
+        state.attach(test_listener)
+        state.blue_scores()
+
+        # then
+        self.assertEqual(len(test_listener.get_captured_events()), 1)
+        self.assertTrue(isinstance(test_listener.get_captured_events()[0], BlueGoal))
+
 
 if __name__ == "__main__":
     main()
