e9d22eb92664f4477c0869893809be750db9fed1
Piotr Grabowski <pgrabowski@sumologic.com>
2019-09-19 11:52:16 +0200

    updated camera processing

--- camera_process.sh
+++ camera_process.sh
@@ -2,18 +2,25 @@
 
 SLACK_TOKEN=$(<slack_token.txt)
 
-rm -f $1.mp4
 MP4Box -fps 60 -add $1.h264:fps=60 $1.mp4
+rm -f $1.h264
+
 ffmpeg \
   -i $1.mp4 \
-  -r 15 \
-  -vf scale=384:-1 \
-  -ss 00:00:15 \
-  $1.gif
-rm -f $1.h264
+  -r 5 \
+  -ss 00:00:25 \
+  -to 00:00:33 \
+  -filter:v "scale=512:-1, setpts=2*PTS" \
+  $1.nonoptimized.gif
+
+gifsicle -i $1.nonoptimized.gif --colors 64 --optimize=3 -o $1.gif
+rm -f $1.nonoptimized.gif
+
 aws s3 cp $1.mp4 s3://foosball-replays/ --acl public-read
-aws s3 cp $1.gif s3://foosball-replays/ --acl public-read
-echo "https://foosball-replays.s3.eu-central-1.amazonaws.com/$1.mp4"
-curl -X POST -H 'Content-type: application/json' --data "{\"channel\": \"pl-foosball-hacking-replays\", \"text\":\"https://foosball-replays.s3.eu-central-1.amazonaws.com/$1.mp4\"}" "https://sumologic.slack.com/services/hooks/jenkins-ci?token=$SLACK_TOKEN"
 rm -f $1.mp4
+
+aws s3 cp $1.gif s3://foosball-replays/ --acl public-read
 rm -f $1.gif
+
+echo "https://foosball-replays.s3.eu-central-1.amazonaws.com/$1.mp4"
+curl -X POST -H 'Content-type: application/json' --data "{\"channel\": \"pl-foosball-hacking-replays\", \"text\": \":soccer: GOAL! :soccer: \n See replay: https://foosball-replays.s3.eu-central-1.amazonaws.com/$1.mp4\", \"attachments\": [{\"fallback\": \"GIF\", \"text\": \"Replay GIF\", \"image_url\": \"https://foosball-replays.s3.eu-central-1.amazonaws.com/$1.gif\"}]}" "https://sumologic.slack.com/services/hooks/jenkins-ci?token=$SLACK_TOKEN"
